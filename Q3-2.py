80815027ae066e18af2fd4e7855fb615fe3c2708
Ivyna Alves <ivynamagalhaes003@gmail.com>
2023-05-29 15:35:59 -0300

    -

--- Q3-2.py
+++ Q3-2.py
@@ -1,6 +1,7 @@
-maior=0
+maior = 0
 for i in range(5):
-    num=int(input("Valor: "))
+    num = int(input("Valor: "))
     if num > maior:
         maior = num
 print("O maior numero é: ", maior)
+print("Maior numero é: ", maior)
